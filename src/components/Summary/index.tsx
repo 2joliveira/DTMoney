457ff9d7e5292c15b39f88b05dbfdba7844e39b4
Jefferson Oliveira <oliveira2112jj@outlook.com>
2021-04-06 17:38:41 -0300

    Finalizando aplicação

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,4 @@
-import React, { useContext } from 'react';
-
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import icomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -9,7 +7,7 @@ import totalImg from '../../assets/total.svg';
 import { Container } from "./styles";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   const sumary = transactions.reduce((acc, transaction) => {
     if (transaction.type === 'deposit') {
